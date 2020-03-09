ccb6b896a9aa74b6b6469acad2d045392deb4086
solson163 <solson163@student.cvtc.edu>
2020-03-09 11:20:54 -0500

    question 2

--- app/src/main/java/com/example/pokemonquiz_solson163/QuestionTwoActivity.java
+++ app/src/main/java/com/example/pokemonquiz_solson163/QuestionTwoActivity.java
@@ -3,11 +3,11 @@ package com.example.pokemonquiz_solson163;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class questionOneActivity extends AppCompatActivity {
+public class QuestionTwoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question_one);
+        setContentView(R.layout.activity_question_two);
     }
 }
